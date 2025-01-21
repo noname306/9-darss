b35f8c67afb5e8d72ff7c72477ac82de070caa70
noname306 <ya.sitora11@gmail.com>
2025-01-21 18:53:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const ism = prompt ("Ismingizni kiriting: ");
+const fam = prompt ("Familiyangizni kiriting: ");
+const yosh = +prompt ("Yoshingizni kiriting: ");
+const harf = +prompt("Ismingiz katta harf yoki kichikligini tanlang \n 1)Katta \n 2)Kichik")
+
+
